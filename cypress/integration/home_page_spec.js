a62a994ce725f22deb4c68fceb76f619fce0db84
isaac <273070111@qq.com>
2020-01-11 10:15:18 +0800

    [ADD]增加demo

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,5 +2,37 @@ describe('The Home Page', function() {
     it('successfully loads', function() {
         // 这里访问相对地址 是在项目根目录的cypress.json中配置了默认的baseUrl
         cy.visit('/')
+        cy.get('#main-content')
+            .find('.article')
+            .children('img')
+            .first()
+            // css的断言写法
+            .should('have.css', 'width', '50px')
+    })
+    it('interact', function() {
+        cy.get('.inputArea')
+            .find('input')
+            // 聚焦
+            .focus()
+            // 输入
+            .type('this is a testing text')
+            // 清空
+            .clear()
+            // 失焦
+            .blur()
+        cy.get('.checkBoxArea')
+            .children('input')
+            // 遍历
+            .then(($element) => {
+                cy.get($element)
+                // 选中
+                .check()
+                // 不选中
+                .uncheck()
+            })
+        cy.get('.selectArea')
+            .find('select')
+            // 选择属性为volvo2的option
+            .select('volvo2')
     })
 })
--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main-content">
+        <div className="article">
+          <img style={{ width: 50, height: 50 }} src={logo} alt=""/>
+          <img style={{ width: 50, height: 50 }} src={logo} alt=""/>
+        </div>
+        <div className="inputArea">
+          <input type="text" id="selfInput"/>
+        </div>
+        <div className="checkBoxArea">
+          <input type="checkbox" name="1" value="bike"/>bike
+          <input type="checkbox" name="1" value="car"/>car
+        </div>
+        <div className="selectArea">
+          <select>
+            <option value="volvo">volvo</option>
+            <option value="volvo2">volvo2</option>
+            <option value="volvo3">volvo3</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
